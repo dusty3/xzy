73a3fe5c541621f1c18c7b019421c0fa3b48f1f4
xuzhangyue <xuzhangyue@163.com>
2018-06-20 00:14:09 +0800

    新建客户表sql   新增两条数据

--- web/WEB-INF/sql/客户表/cm_customer.sql
+++ web/WEB-INF/sql/客户表/cm_customer.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50546
 File Encoding         : 65001
 
-Date: 2018-06-19 23:55:37
+Date: 2018-06-20 00:11:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -46,8 +46,10 @@ CREATE TABLE `cm_customer` (
   `customer_modifytime` datetime DEFAULT NULL,
   `customer_dr` int(11) NOT NULL DEFAULT '0' COMMENT '默认为0  ， 1是删除',
   PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of cm_customer
 -- ----------------------------
+INSERT INTO `cm_customer` VALUES ('1', '常州嬉戏谷有限公司', '常州嬉戏谷有限公司', '9132041258558635XF', '招商银行', '0123000103000008865', '051088550001', '常州市武进区雪堰镇太北路1号', '夏楠', '13806182806', 'mia@demeter7.com', '常州市武进区雪堰镇太北路1号', '夏楠', '13806182806', 'mia@demeter7.com', '常州市武进区雪堰镇太北路1号', '夏楠', '13806182806', 'mia@demeter7.com', '花生', '张浩一', '没有', '2018-06-13 00:08:26', null, '0');
+INSERT INTO `cm_customer` VALUES ('2', '埃梯梯精密机械制造（无锡）有限公司', '埃梯梯精密机械制造（无锡）有限公司', '91320214789913869F', '中国银行', '0123000103000008835', '051088550000', '无锡新区梅村锡达路570号', 'Vincent Zhang \r\nVincent Zhang \r\nVincent Zhang ', '13806182805', 'vincent.zhang@itt.com', '无锡新区梅村锡达路570号', 'Vincent Zhang ', '13806182805', 'vincent.zhang@itt.com', '无锡新区梅村锡达路570号', 'Vincent Zhang ', '13806182805', 'vincent.zhang@itt.com', '杨凯', '杨磊', '不要用公司胶带', '2018-06-20 00:08:21', null, '0');
